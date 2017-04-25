acc846da8cd7fc9e3b3eaf92db8b8823e22ca8a0
João Pires Barreira <jpiresbarreira@gmail.com>
2017-04-25 03:26:35 +0100

    Pequenas alterações no program.c

--- program.c
+++ program.c
@@ -44,10 +44,8 @@ int main(int argc, char* argv[]) {
 			printf("\t%d  - %ld\n", i+1, res4[i]);
 	}
 
-	printf("\nQ5: \t%s\n", contributor_name(28903366, qs));
-	printf("Q5: \t%s\n", contributor_name(194203, qs));
-	printf("Q5: \t%s\n\n", contributor_name(1000, qs));
-
+	printf("\nQ5: \t%s\n\n", contributor_name(194203, qs));
+	
 	printf("Q6:");
     long* res6 = top_20_largest_articles(qs);
 	for (int i = 0; i < 20; i++) { 
@@ -58,13 +56,11 @@ int main(int argc, char* argv[]) {
 	}
 
 
-	printf("\nQ7: \t%s\n", article_title(15910, qs));
-	printf("Q7: \t%s\n", article_title(25507, qs));
-	printf("Q7: \t%s\n\n", article_title(1111, qs));
-
+	printf("\nQ7: \t%s\n\n", article_title(15910, qs));
+	
 	printf("Q8:");
-	long* res8 = top_N_articles_with_more_words(30, qs);
-	for (int i = 0; i < 30; i++) {
+	long* res8 = top_N_articles_with_more_words(10, qs);
+	for (int i = 0; i < 10; i++) {
 		if (i+1 >= 10)
 			printf("\t%d - %ld\n", i+1, res8[i]);
 		else
@@ -76,8 +72,6 @@ int main(int argc, char* argv[]) {
     for (int i = 0; res9[i] != NULL; i++) printf("\t%s\n", res9[i]);
 
 	printf("\nQ10: \t%s\n", article_timestamp(12, 763082287, qs));
-	printf("Q10: \t%s\n", article_timestamp(12, 755779730, qs));
-	printf("Q10: \t%s\n", article_timestamp(12, 4479730, qs));
 
 
 	// Clean
