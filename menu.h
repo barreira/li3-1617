5372fca0b851b7a55a2305865162e05466d81a9f
Joana Arantes <frize_91@hotmail.com>
2017-05-01 04:00:41 +0100

    Adicionado comentários

--- menu.h
+++ menu.h
@@ -1,7 +1,26 @@
+/**
+ * @file menu.h
+ * @brief API do módulo do menu
+ *
+ * @author Ana Paula Carvalho  - A61855
+ * @author Joana Arantes       - A57810
+ * @author João Pires Barreira - A73831 
+ * @author Miguel Cunha        - A78478
+ *
+ * @version 2017-04-28
+ */
+
 #ifndef MENU_H
 #define MENU_H
 
-/* API */
+/******************************************************************************
+ *                                 API                                        *
+ ******************************************************************************/
+
+/*
+ * @brief Inicializa o menu (a execução do programa).
+ *
+ */
 
 int menu();
 
