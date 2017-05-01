457550b3e76c9ff5ccd568865bc620b91bdc9694
Joana Arantes <frize_91@hotmail.com>
2017-05-01 04:16:42 +0100

    Adicionado comentários

--- menu.c
+++ menu.c
@@ -1,3 +1,18 @@
+/**
+ * @file menu.c
+ * @brief Módulo responsável pela interface gráfica do programa, permitindo assim a sua facilidade no teste e excução.
+ * 
+ * Está feito e pensado para que, quando se quiser executar uma querie, apenas com o número da querie,
+ * o menu chame a querie.
+ *
+ * @author Ana Paula Carvalho  - A61855
+ * @author Joana Arantes       - A57810
+ * @author João Pires Barreira - A73831 
+ * @author Miguel Cunha        - A78478
+ *
+ * @version 2017-04-28
+ */
+
 #include "menu.h"
 #include "interface.h"
 #include <stdio.h>
@@ -8,10 +23,24 @@
 #include <stdbool.h>
 #include <time.h>
 
+/*
+ * Variáveis que calculam o tempo que determinada querie demorou a executar (em segundos).
+ */
+
 clock_t start, end;
 double cpu_time_used;
 
+/*
+ * @brief Inicialização da estrutura, para que o apontador seja conhecido (neste caso é vazio).
+ *
+ * @return Apontador NULL
+ */
+
 TAD_istruct s = NULL;
+
+/******************************************************************************
+ *                                        MENU                                *
+ ******************************************************************************/
 
 int menu()
 {
@@ -43,6 +72,15 @@ int menu()
 
     while(sair!=1)
     {   
+
+        /*
+         * @brief Inicializa a estrutura de dados principal do programa.
+         *
+         * Deve ser a primeira função a ser executada pois só após a chamada desta
+         * função, é que a estrutura de dados estará pronta a ser carregada.
+         *
+         */
+
         if (strcmp(opcao, "i") == 0)
         {
             start = clock();
@@ -57,6 +95,14 @@ int menu()
             fflush(stdin);
             menu();
         }
+
+         /*
+         * @brief Carrega a estrutura de dados principal do programa
+         *
+         * A estrutura de dados deve primeiro ser inicializada. O carregamento é feito
+         * a partir dos ficheiros das snapshots cujo caminho é passado como parâmetro.
+         *
+         */
 
         else if (strcmp(opcao, "l") == 0)
         {
@@ -78,11 +124,18 @@ int menu()
             menu();            
         }
 
+        /*
+         * @brief Liberta a estrutura de dados principal do programa
+         *
+         * Liberta o apontador para estrutura passada como parâmetro, bem como todo e
+         * qualquer espaço previamente ocupado pela mesma.
+         *
+         */
+
         else if (strcmp(opcao, "c") == 0)
         {
             start = clock();
 
-            //fazer clean
             s = clean(s);
 
             end = clock();
@@ -94,11 +147,18 @@ int menu()
             menu();            
         }
 
+        /*
+         * @brief Query 1
+         *
+         * Calcula o número total de artigos encontrados, incluindo artigos duplicados
+         * e novas revisões de artigos já existentes. 
+         *
+         */
+
         else if (strcmp(opcao, "1")==0)
         {
         	start = clock();
             
-            //Query 1
             printf("Q1: %ld\n", all_articles(s));
 
             end = clock();
@@ -111,11 +171,19 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 2
+         *
+         * Calcula o número de artigos únicos encontrados (i.e. artigos com
+         * identificador único). Ou seja, não inclui artigos duplicados nem novas
+         * revisões de artigos já existentes.
+         *
+         */
+
         else if (strcmp(opcao, "2")==0)
         {
         	start = clock();
 
-            //Query 2
             printf("Q2: %ld\n", unique_articles(s));
 
             end = clock();
@@ -128,11 +196,19 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 3
+         *
+         * Calcula o número total de revisões de artigos encontradas. Inclui quer a
+         * versão base de um artigo, quer as restantes revisões feitas ao mesmo
+         * posteriormente.
+         *
+         */
+
         else if (strcmp(opcao, "3")==0)
         {
         	start = clock();
 
-            //Query 3
             printf("Q3: %ld\n", all_revisions(s));
             
             end = clock();
@@ -145,11 +221,20 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 4
+         *
+         * Devolve um array com os IDs dos 10 contribuidores que contribuíram para um
+         * maior número de revisões de artigos. O resultado é ordenado de forma
+         * decrescente e, caso, existam contribuidores com um número igual de
+         * contribuições, estes aparecem ordenados pelo seu ID (do menor para o maior).
+         *
+         */
+
         else if (strcmp(opcao, "4")==0)
         {
         	start = clock();
 
-            //Query 4
             int i;
             long* res = top_10_contributors(s);
 
@@ -168,11 +253,18 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 5
+         *
+         * Devolve o username de um contribuidor cujo ID é passado como parâmetro. Caso
+         * não exista nenhum contribuidor com esse ID, é devolvido NULL.
+         *
+         */
+
         else if (strcmp(opcao, "5")==0)
         {
         	start = clock();
             
-            //Query 5
             char id[100];
             printf("Insira um id de um contribuidor: ");
             scanf("%s", id);
@@ -194,11 +286,21 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 6
+         *
+         * Devolve um array com os IDs dos 20 artigos que possuem textos com um maior
+         * tamanho (em bytes). Para cada artigo, é apenas contabilizado o maior tamanho
+         * de todas as revisões do mesmo. O resultado é ordenado de forma decrescente
+         * e, caso, existam artigos com um tamanho igual, estes aparecem ordenados pelo
+         * seu ID (do menor para o maior).
+         *
+         */
+
         else if (strcmp(opcao, "6")==0)
         {
         	start = clock();
 
-            //Query 6
         	int i;
             long* res = top_20_largest_articles(s);
 
@@ -219,11 +321,19 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 7
+         *
+         * Devolve o título de um artigo cujo ID é passado como parâmetro. São apenas
+         * considerados os títulos das últimas revisões dos artigos. Caso não exista
+         * nenhum artigo com esse ID, é devolvido NULL.
+         *
+         */
+
         else if (strcmp(opcao, "7")==0)
         {
             start = clock();
             
-            //Query 7
             char id[100];
             printf("Insira um id de um artigo: ");
             scanf("%s", id);
@@ -246,11 +356,21 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 8
+         *
+         * Devolve um array com os identificadores dos N artigos que possuem textos com
+         * um maior número de palavras. Para cada artigo, é apenas contabilizado o texto
+         * com o maior número de palavras de todas as revisões do mesmo. O resultado é
+         * ordenado de forma decrescente e, caso, existam artigos com um número de
+         * palavras igual, estes aparecem ordenados pelo seu ID (do menor para o maior).
+         *
+         */
+
         else if (strcmp(opcao, "8")==0)
         {
         	start = clock();
             
-            //Query 8
             int tam;
             printf("Insira um tamanho: ");
             scanf("%d", &tam);
@@ -270,11 +390,19 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 9
+         *
+         * Devolve um array com os títulos dos artigos que começam pelo prefixo passado
+         * como parâmetro. É apenas considerado o título da última revisão de cada
+         * artigo.
+         *
+         */
+
         else if (strcmp(opcao, "9")==0)
         {
         	start = clock();
 
-            //Query 9
             char prefix[100];
             printf("Insira um prefixo: ");
             scanf("%s", prefix);
@@ -305,11 +433,18 @@ int menu()
             menu();
         }
 
+        /*
+         * @brief Query 10
+         *
+         * Devolve o timestamp de uma revisão de um dado artigo (valores passados como
+         * parâmetro).
+         *
+         */
+
         else if (strcmp(opcao, "10")==0)
         {
         	start = clock();
 
-            //Query 10
             char a_id[100];
             printf("Insira um id de um artigo: \n");
             scanf("%s", a_id);
@@ -335,10 +470,23 @@ int menu()
             sair=1;
             fflush(stdin);
             menu();
-        }       
+        } 
+
+        /*
+         * @brief Termina a execução do programa.
+         *
+         */      
 
         else if (strcmp(opcao, "0")==0)
             sair=1;
+
+        /*
+         * @brief Opção inserida não é válida.
+         *
+         * Tem de se introduzir um valor entre 0 e 10 ou os caracteres i, l ou c para que
+         * a opção do menu seja executada.
+         * 
+         */
 
         else
         {
